89523b7122b3db7093eb82698a0c90133a551b57
escos <rv_kireev@bk.ru>
2016-10-26 16:42:50 +0300

    Circle_changing_ver.4

--- src/ru/levelp/myzoo/Zoo.java
+++ src/ru/levelp/myzoo/Zoo.java
@@ -1,14 +1,16 @@
 package ru.levelp.myzoo;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
+
 
 public class Zoo {
     static Scanner sc = new Scanner(System.in);
     static Random gen = new Random();
+    final private static int N = 5;
 
     public static Animal[] createZoo() {
-        int N = 6;
         Animal[] animal_mass = new Animal[N];
         for (int i = 0; i < N; i++) {
             boolean var = gen.nextBoolean();
@@ -21,34 +23,29 @@ public class Zoo {
             } else {
                 kind = "хищное";
                 String[] women_mass = {"Тигр", "Волк", "Лев", "Крокодил"};
-                name = women_mass[gen.nextInt(5)];
+                name = women_mass[gen.nextInt(4)];
             }
             String[] col_arr = {"черный", "белый", "коричневый", "серый", "рыжий"};
             String[] gender_arr = {"самка", "самец"};
             String color = col_arr[gen.nextInt(5)];
             String gender = gender_arr[gen.nextInt(2)];
             System.out.println((i + 1) + " сбежавший представитель зоопарка это " + kind + " животное " + name);
-            System.out.println(name + " имеет " + color + " окрас, а также "+name+" - " + gender);
+            System.out.println(name + " имеет " + color + " окрас, а также " + name + " - " + gender);
         }
         return animal_mass;
     }
 
-    public static Animal paring(Animal arr1, Animal arr2) {
-        boolean kind = arr1.kind;
-        String name = arr1.name;
-        Animal child = new Animal(name, kind);
-
-        if (arr1.color == arr2.color) {
-            child.color = arr1.color;
-        } else {
-            if (arr1.gender == "самка") {
-                child.color = arr1.color;
-            } else {
-                child.color = arr2.color;
+    public static void herbivoreSort(Animal arr[]) {
+        ArrayList<String> herbiNames = new ArrayList<String>();
+        int count = 0;
+        int i;
+        for (i = 0; i < arr.length; i++)
+            if (arr[i].kind) {
+                count += 1;
+                herbiNames.add(arr[i].name);
             }
-        }
-        System.out.println("В результате спаривания может получится " + name + " " + child.color + " цвета");
-        return child;
+        System.out.println("Количество сбежавших травоядных: " + count);
+        System.out.println("Список сбежавших травоядных:" + herbiNames);
     }
 }
 
